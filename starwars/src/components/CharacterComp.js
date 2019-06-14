4635a94e780ff134b68b7618e836d53828ff842c
Melanie Harris <mell.j.harris@gmail.com>
2019-06-14 14:36:53 -0700

    Applying additonal styling, but MVP met

--- starwars/src/components/CharacterComp.js
+++ starwars/src/components/CharacterComp.js
@@ -8,8 +8,15 @@ const CharacterComp = (props) => {
     return (
         <div className="character-card">
             {props.characters.map(item =>
-                <h2>{item.name}</h2>
-                
+                <div>
+                    <h2>{item.name}</h2>
+                    <h2><span className="title">Birth Year: </span> {item.birth_year}</h2>
+                    <h2><span className="title">Mass: </span> {item.mass}</h2>
+                    <h2><span className="title">Height: </span> {item.height}</h2>
+                    <h2><span className="title"> Hair Color: </span> {item.hair_color}</h2>
+                    <br />
+                </div>
+
 
             )};
         </div>
